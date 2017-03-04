762df7384a54d067e158ceb91feeaa0d82b134e9
Klongrich <longrichk@gmail.com>
2017-03-04 06:35:27 -0800

    Debuging

--- main/driver.java
+++ main/driver.java
@@ -5,9 +5,11 @@ package main;
  */
 import data.qoutes;
 import data.UpDateDatabase;
+import graph.livetickers;
 import indicators.listone;
 import graph.Graph;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class driver {
@@ -15,11 +17,37 @@ public class driver {
     public static void main (String [] args)
     {
         listone inda = new listone();
-        qoutes data = new qoutes("SPY", 60, 2);
+        qoutes data = new qoutes("SPY", 300, 10);
 
         Graph chart = new Graph("SPY");
-        //chart.addIndicator(inda.sma(data.low(), 50));
+        //chart.addIndicator(inda.fsma(data.smoothed(), 20));
+        //chart.addIndicator(inda.bsma(data.smoothed(), 20));
 
+        livetickers x = new livetickers("SPY");
+        //livetickers y = new livetickers("DOW");
+
+        while (true) {
+            double startTime = System.currentTimeMillis();
+            x.volume();
+            System.out.println(x.price());
+            double endTime = System.currentTimeMillis();
+            double totalTime = endTime - startTime;
+
+            DecimalFormat dfseconds = new DecimalFormat("#.00");
+            DecimalFormat dfminutes = new DecimalFormat("#");
+            double minutes;
+            double seconds;
+
+            if ((totalTime / 1000) < 60) {
+                System.out.println("Calculated in " + dfseconds.format(totalTime / 1000) + " seconds");
+            } else {
+                minutes = (totalTime / 1000) / 60;
+                seconds = (totalTime / 1000) % 60;
+
+                System.out.println("Calculated in " + dfminutes.format(minutes) + " minutes " + dfseconds.format(seconds) + " seconds");
+            }
+        }
+        //System.out.println(x.yesterdayClose("spy"));
     }
 
 }
