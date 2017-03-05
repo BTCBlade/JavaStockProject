efd17394de39390bc722bfa111dac39b52bb6189
Klongrich <longrichk@gmail.com>
2017-03-04 20:16:24 -0800

    Working on indicators

--- indicators/listone.java
+++ indicators/listone.java
@@ -44,4 +44,48 @@ public class listone {
         return (answer);
     }
 
+    public ArrayList<Double> ema(ArrayList <Double> data, int period)
+    {
+        ArrayList <Double> answer = new ArrayList <Double>();
+        double mulit;
+        double temp;
+        double start;
+
+        temp = 0;
+        start = 0;
+        mulit = (double)2 / (period + 1);
+        for (int j = 0; j < period; j++)
+        {
+            start += data.get(j);
+        }
+        start /= period;
+        answer.add((data.get(0) - start) * mulit + start);
+        temp = start;
+        for (int i = 1; i < data.size() - period; i++)
+        {
+            temp = (data.get(i) - temp) * mulit + temp;
+            answer.add(temp);
+        }
+        return (answer);
+    }
+
+    public ArrayList <Double> macd(ArrayList<Double> data, int period1, int period2)
+    {
+        ArrayList <Double> answer = new ArrayList <Double>();
+        ArrayList <Double> ema1 = new ArrayList<Double>();
+        ArrayList <Double> ema2 = new ArrayList<Double>();
+        int len;
+
+        ema1 = ema(data, period1);
+        ema2 = ema(data, period2);
+        if (ema1.size() < ema2.size())
+            len = ema1.size();
+        else
+            len = ema2.size();
+        for (int i = 0; i < len; i++)
+        {
+            answer.add(ema1.get(i) - ema2.get(i));
+        }
+        return (answer);
+    }
 }
