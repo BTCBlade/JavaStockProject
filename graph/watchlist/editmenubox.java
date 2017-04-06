32e49f60c0e86ee63ba960803038af65175e0435
Kyle LONGRICH <klongric@e1z3r10p7.42.us.org>
2017-04-05 23:41:05 -0700

    Used for the frame in the edit menu box

---
+++ graph/watchlist/editmenubox.java
@@ -0,0 +1,30 @@
+package graph.watchlist;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+/**
+ * Created by klongric on 3/29/17.
+ */
+public class editmenubox extends JDialog {
+
+    editmenu menu;
+
+
+    public editmenubox(ArrayList<String> tickers)
+    {
+        editmenu menu = new editmenu(tickers);
+        this.menu = menu;
+        setLayout(null);
+        menu.setLocation(0, 0);
+        setSize(250, 500);
+        add(menu);
+        setVisible(true);
+        setResizable(false);
+        setLocationRelativeTo(getParent());
+    }
+
+}
